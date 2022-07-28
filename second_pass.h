6f1d3279c89d79785b2ae74333c248acc69e5fe3
Or Badani <41251785+orbad@users.noreply.github.com>
2022-07-28 17:39:47 +0300

    Update second_pass.h

--- second_pass.h
+++ second_pass.h
@@ -14,27 +14,29 @@ typedef struct extern_addresses {
 } extAddress;
 
 /*enums for navigating through the different binary translating processes*/
-enum operation{funct_op=15, source_reg_op=11, source_add_op=7, dest_reg_op=5, dest_add_op=1};
+enum operation{opcode_add=9, source_add_op=5, dest_add_op=3};
 
-enum operation_length{funct_len=4, reg_len=4, add_len=2};
+enum operation_length{add_len=2};
 
 enum addressing{immediate = 0 , direct=1, index=2, reg_direct=3 };
 
-enum full_binary_translation{binary_line_len=16, binary_line_ind=15 };
+enum full_binary_translation{binary_line_len=10, binary_line_ind=9 };
 
 /*for finding the indexes in the value array */
 enum values_Array_index{errorFlag_ind = 0 , printCounter_ind=1 ,extAdd_ind=2, value_Array_Size=3};
 
 
-/*functions for translating to bits, handling each operand and each line functions */
+/* Functions for translating to bits, handling each operand and each line's functions */
 
 int firstWordProcess(int action, char line[], FILE *newCode, int printCounter);
 
 void bitTranslation(int bitLength, int index, int value, char line[]);
 
-int stringTranslation(char * token, dataList *dataLine,int DC);
+int stringTranslation(char * token, dataList *dataLine, int DC);
 
-int dataTranslation(char * token, dataList *dataLine,int DC);
+int structTranslation(char * token, dataList *dataLine, int DC); /* Ongoing Process */
+
+int dataTranslation(char * token, dataList *dataLine, int DC);
 
 int* middleWordProcess(char *firstOperand, char *secondOperand, int funct, symbol *symTable, int symTableSize, FILE *newCode,extAddress *extAdd,int extSize, int printCounter,int errorFlag);
 
