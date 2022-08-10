1b3c447a75fcfd3f9309ea2cdb294aee26a413cb
Or Badani <41251785+orbad@users.noreply.github.com>
2022-08-10 21:53:04 +0300

    Update first_pass.h

--- first_pass.h
+++ first_pass.h
@@ -5,8 +5,6 @@ enum memory_index{mem_data_ind=34 ,mem_cmd_ind=32, mem_reg_ind=8 };
 
 /*symbol table functions */
 
-int isValidSymbol(string label, symbol * symTable, int tableSize, sysReserved *memory, int lineCounter);
+int isValidSymbol(String label, symbol * symTable, int tableSize, sysReserved *memory, int lineCounter);
 
 void addSymbol(String label, int address, int source, int tableSize, symbol *symTable);
-
-
